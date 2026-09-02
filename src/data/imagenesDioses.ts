2601357a981f084f68629852572f4c7502ee87ea
Ferbeatriz <ferbeatriz@proton.me>
2026-09-02 14:56:38 -0400

    Agregadas imágenes GIF de los dioses egipcios

---
+++ src/data/imagenesDioses.ts
@@ -0,0 +1,11 @@
+// src/data/imagenesDioses.ts
+
+export const imagenesDioses: Record<string, string> = {
+  "AMÓN": "/images/dioses/amon.gif",
+  "ANUBIS": "/images/dioses/anubis.gif",
+  "ANUKIS": "/images/dioses/anukis.gif",
+  "APIS": "/images/dioses/apis.gif",
+  "ATÓN": "/images/dioses/aton.gif",
+  "ATUM": "/images/dioses/atum.gif",
+  // ... agrega todos los dioses que tengas
+};
